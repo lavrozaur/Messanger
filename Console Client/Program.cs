b13872a64f0b95a42384ae8d5a48adedd609f4ea
lavrozaur <64269407+lavrozaur@users.noreply.github.com>
2021-08-19 11:21:06 +0300

    Простой консольный клиент

--- Console Client/Program.cs
+++ Console Client/Program.cs
@@ -5,15 +5,37 @@ namespace Messanger
 {
     class Program
     {
+        private static int MessageID;
+        private static string UserName;
+        private static MessangerClientAPI API = new MessangerClientAPI();
+
+        private static void GetNewMessages()
+        {
+            Message msg = API.GetMessage(MessageID);
+            while (msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
         static void Main(string[] args)
         {
-            Message msg = new Message("Pavel","Privet",DateTime.UtcNow);
-            string output = JsonConvert.SerializeObject(msg);
-            Console.WriteLine(output);
-            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
-            Console.WriteLine(deserializedMsg);
-            //{ "UserName":"Pavel","MessageText":"Privet","TimeStamp":"2021-08-15T10:44:07.5976314Z"}
-           // Pavel < 15.08.2021 10:44:07 >: Privet
+            MessageID = 1;
+            Console.WriteLine("Введите Ваше имя:");
+            //UserName = "RusAl";
+            UserName = Console.ReadLine();
+            string MessageText = "";
+            while (MessageText != "exit")
+            {
+                GetNewMessages();
+                MessageText = Console.ReadLine();
+                if (MessageText.Length > 1)
+                {
+                    Message Sendmsg = new Message(UserName, MessageText, DateTime.Now);
+                    API.SendMessage(Sendmsg);
+                }
+            }
 
         }
     }
