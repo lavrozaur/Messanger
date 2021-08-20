3c6e4dd6f188925b8f1fe9a0a2f8003662519daf
lavrozaur <64269407+lavrozaur@users.noreply.github.com>
2021-08-20 20:36:53 +0300

    Подключение RestSharp и визуализации на основе WinForm, а так же асинхронных функций

---
+++ WindowsFormsClient/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace WindowsFormsClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.MessageLB = new System.Windows.Forms.ListBox();
+            this.UserNameTB = new System.Windows.Forms.TextBox();
+            this.MessageTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // SendButton
+            // 
+            this.SendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SendButton.Location = new System.Drawing.Point(707, 408);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(261, 121);
+            this.SendButton.TabIndex = 0;
+            this.SendButton.Text = "Отправить";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // MessageLB
+            // 
+            this.MessageLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MessageLB.FormattingEnabled = true;
+            this.MessageLB.ItemHeight = 29;
+            this.MessageLB.Location = new System.Drawing.Point(12, 12);
+            this.MessageLB.Name = "MessageLB";
+            this.MessageLB.Size = new System.Drawing.Size(956, 265);
+            this.MessageLB.TabIndex = 1;
+            // 
+            // UserNameTB
+            // 
+            this.UserNameTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.UserNameTB.Location = new System.Drawing.Point(12, 408);
+            this.UserNameTB.Name = "UserNameTB";
+            this.UserNameTB.Size = new System.Drawing.Size(214, 49);
+            this.UserNameTB.TabIndex = 2;
+            // 
+            // MessageTB
+            // 
+            this.MessageTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MessageTB.Location = new System.Drawing.Point(12, 507);
+            this.MessageTB.Name = "MessageTB";
+            this.MessageTB.Size = new System.Drawing.Size(605, 49);
+            this.MessageTB.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 376);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 475);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(217, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Текст сообщения";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 585);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MessageTB);
+            this.Controls.Add(this.UserNameTB);
+            this.Controls.Add(this.MessageLB);
+            this.Controls.Add(this.SendButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.ListBox MessageLB;
+        private System.Windows.Forms.TextBox UserNameTB;
+        private System.Windows.Forms.TextBox MessageTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ WindowsFormsClient/Form1.cs
@@ -0,0 +1,52 @@
+﻿
+using Messanger;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsClient
+{
+    public partial class Form1 : Form
+    {
+        private static int MessageID = 0;
+        private static string UserName;
+        private static MessangerClientAPI API = new MessangerClientAPI();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            string UserName = UserNameTB.Text;
+            string Message = MessageTB.Text;
+            if ((UserName.Length > 1) && (UserName.Length > 1))
+            {
+                Messanger.Message msg = new Messanger.Message(UserName, Message, DateTime.Now);
+                API.SendMessageRestSharp(msg);
+            }
+
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            var getMessage = new Func<Task>(async () =>
+            {
+                Messanger.Message msg = await API.GetMessageHTTPAsync(MessageID);
+                while (msg != null)
+                {
+                    MessageLB.Items.Add(msg);
+                    MessageID++;
+                    msg = await API.GetMessageHTTPAsync(MessageID);
+                }
+            });
+            getMessage.Invoke();
+        }
+    }
+}
