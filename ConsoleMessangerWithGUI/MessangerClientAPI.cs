8a54c7b5180e9d03b1f5083332857993e1bf5c57
lavrozaur <64269407+lavrozaur@users.noreply.github.com>
2021-09-06 21:52:48 +0300

    Добавили GUI

---
+++ ConsoleMessangerWithGUI/MessangerClientAPI.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messanger
+{
+    class MessangerClientAPI
+    {
+        public void TestNewtonsoftJson()
+        {
+            Message msg = new Message("Pavel", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //{ "UserName":"Pavel","MessageText":"Privet","TimeStamp":"2021-08-15T10:44:07.5976314Z"}
+            // Pavel < 15.08.2021 10:44:07 >: Privet
+            // Cоздание файла
+            //string path = @"d:\temp\ser.txt";
+            //using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+            //{
+            //  sw.WriteLine(output);
+            //}
+        }
+        public Message GetMessage(int MessageId)
+        {
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/Mess/" + MessageId.ToString());
+            request.Method = "Get";
+            WebResponse response = request.GetResponse();
+            string status = ((HttpWebResponse)response).StatusDescription;
+            //Console.WriteLine(status);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            //Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            if ((status.ToLower() == "ok") && (responseFromServer != "Not found"))
+            {
+                Message deserializedMsg = JsonConvert.DeserializeObject<Message>(responseFromServer);
+                //Console.WriteLine(deserializedMsg);
+                return deserializedMsg;
+            }
+            return null;
+        }
+        public bool SendMessage(Message msg)
+        {
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/Mess");
+            request.Method = "POST";
+            //Message msg = new Message("RusAl", "Privet1100", DateTime.Now);
+            string postData = JsonConvert.SerializeObject(msg);
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            WebResponse response = request.GetResponse();
+            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            //Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return true;
+        }
+
+    }
+}
